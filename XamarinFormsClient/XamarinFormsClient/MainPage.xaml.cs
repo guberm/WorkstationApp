63704151bd2302ed6a1e68017254a7bad9da08f3
Michael Guber <guberm@gmail.com>
2020-02-17 23:24:52 -0500

    Initial commit

---
+++ XamarinFormsClient/XamarinFormsClient/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinFormsClient.Client;
+
+namespace XamarinFormsClient
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Connect_Clicked(object sender, EventArgs e)    
+        {    
+            try    
+            {    
+                TcpClient client = new TcpClient();     
+                await client.ConnectAsync(IPAddress.Text, Convert.ToInt32(Port.Text));    
+                if (client.Connected)    
+                {    
+                    Connection.Instance.client = client;    
+                    Application.Current.MainPage = new NavigationPage(new OperationsPage());    
+                        
+                    await DisplayAlert("Connected", "Connected to server successfully!", "Ok");    
+                }    
+                else    
+                {    
+                    await DisplayAlert("Error", "Connection unsuccessful!", "Ok");    
+                }    
+            }    
+            catch (Exception ex)    
+            {    
+                await DisplayAlert("Error", ""+ex.ToString(), "Ok");    
+            }    
+        } 
+    }
+}
---
+++ XamarinFormsClient/XamarinFormsClient/OperationsPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinFormsClient.Client;
+
+namespace XamarinFormsClient
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OperationsPage : ContentPage
+    {
+        public OperationsPage()
+        {
+            InitializeComponent();
+        }
+
+        // Sleep
+        private void Sleep_Clicked(object sender, EventArgs e)  
+        {  
+            TcpClient client = Connection.Instance.client;  
+            NetworkStream stream = client.GetStream();  
+            String s = "SLP2";  
+            byte[] message = Encoding.ASCII.GetBytes(s);  
+            stream.Write(message, 0, message.Length);  
+        }  
+ 
+        // Shutdown
+        private void Shutdown_Clicked(object sender, EventArgs e)  
+        {  
+            TcpClient client = Connection.Instance.client;  
+            NetworkStream stream = client.GetStream();  
+            String s = "SHTD3";  
+            byte[] message = Encoding.ASCII.GetBytes(s);  
+            stream.Write(message, 0, message.Length);  
+        }  
+ 
+        // Screenshot
+        private void Screenshot_Clicked(object sender, EventArgs e)  
+        {  
+            TcpClient client = Connection.Instance.client;  
+            NetworkStream stream = client.GetStream();  
+            String s = "TSC1";  
+            byte[] message = Encoding.ASCII.GetBytes(s);  
+            stream.Write(message, 0, message.Length);  
+            byte[] data = getData(client);  
+            imageView.Source = ImageSource.FromStream(() => new MemoryStream(data));  
+        }  
+  
+        // Get data from server
+        public byte[] getData(TcpClient client)  
+        {  
+            NetworkStream stream = client.GetStream();  
+            byte[] fileSizeBytes = new byte[4];  
+            int bytes = stream.Read(fileSizeBytes, 0, fileSizeBytes.Length);  
+            int dataLength = BitConverter.ToInt32(fileSizeBytes, 0);  
+  
+            int bytesLeft = dataLength;  
+            byte[] data = new byte[dataLength];  
+  
+            int buffersize = 1024;  
+            int bytesRead = 0;  
+  
+            while (bytesLeft > 0)  
+            {  
+                int curDataSize = Math.Min(buffersize, bytesLeft);  
+                if (client.Available < curDataSize)  
+                    curDataSize = client.Available;  
+                bytes = stream.Read(data, bytesRead, curDataSize);  
+                bytesRead += curDataSize;  
+                bytesLeft -= curDataSize;  
+            }  
+            return data;  
+        }  
+    }
+}
